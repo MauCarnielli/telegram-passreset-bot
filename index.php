fe1b6e0bb155c99d507ff87227c8e1cd96f54113
MauCarnielli <mau.carnielli@gmail.com>
2018-08-14 13:45:42 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,54 +1,19 @@
-<?php 
-
-	echo "Alo?";
-
-	define('TOKEN','637138978:AAGPr-IXTHJ6T41qcChyogonTyXcbblEpgw');
-	define('API','https://api.telegram.org/bot'.TOKEN.'/');
+<?php
 	
-	function processMessage($message){
-		$message_id = $message['message_id'];
-		$chat_id = $message['chat']['id'];
-		
-		//Checa se a mensagem não está vazia
-		if(isset($message['text'])){
-			$text = $message['text']; //Atribui a mensagem a váriavel $text
+	$api = "https://api.telegram.org/bot637138978:AAGPr-IXTHJ6T41qcChyogonTyXcbblEpgw/";
+	
+	function processaMensagem($mensagem){
+		$message_id = $mensagem['message_id'];
+		$chat_id = $mensagem['chat']['id'];
+		if(isset($mensagem['text'])){
+			$text = $mensagem['text'];
 		}
-		
-		if(strpos($text, "/start") === 0){
-			
-			sendMessage("sendMessage", array('chat_id'=>$chat_id, "text"=>'OPA MLK');
-		}
-		
-		else if($text == "Oi"){
-			sendMessage("sendMessage", array('chat_id'=>$chat_id, "text"=>"Deu certo, Maurício.");
-		}
+	}
+	
+	function mandaMensagem($metodo, $parametros){
 		
 	}
 	
-	function sendMessage($method, $parameters){
-		$options = array(
-			'http' => array(
-				'method' => 'POST',
-				'content' => json_encode($parameters),
-				'header' => "Content-Type: application/json\r\n" .
-							"Accept: application/json\r\n"
-			)
-		);
-		
-		$context = stream_context_create($options);
-		file_get_contents(API.$method, false, $context);
-	}
-
-	$update_response = file_get_contents("php://input");
-	
-	$update = json_decode($update_response, true);
-	
-	if(isset($update["message"])){
-		processMessage($update["message"]);
-		echo "DEU";
-	}else{
-		echo "NAO DEU";
-	}
-	
+	echo "TESTE";
 	
 ?>
